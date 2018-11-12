4aece4726d19707dfd73a53360ca8d513abf1882
Gray Smith <gray.smith00@gmail.com>
2018-11-12 16:45:52 -0700

    Add iterative solution to factorial

--- practice/factorial.js
+++ practice/factorial.js
@@ -3,6 +3,14 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
+// function factorial(n) {
+//   let num = 1;
+//   for (let i = n; i > 0; i--) {
+//     num *= i;
+//   }
+//   return num;
+// }
+
 // factorial(0) // 1
 // factorial(1) // 1
 console.log(factorial(6)); // 720
