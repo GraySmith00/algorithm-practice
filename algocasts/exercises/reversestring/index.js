672a2279822c4ad50dc61721ad6ba9f7956f4828
Gray Smith <gray.smith00@gmail.com>
2018-11-14 19:13:22 -0700

    Add more solutions to algocasts/reversestring

--- algocasts/exercises/reversestring/index.js
+++ algocasts/exercises/reversestring/index.js
@@ -6,12 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   let revStr = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     revStr += str[i];
+//   }
+//   return revStr;
+// }
+
+// function reverse(str) {
+//   let revStr = '';
+//   for (let char of str) {
+//     revStr = char + revStr;
+//   }
+//   return revStr;
+// }
+
 function reverse(str) {
-  let newStr = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    newStr += str[i];
-  }
-  return newStr;
+  return str.split('').reduce((revStr, char) => {
+    return char + revStr;
+  }, '');
 }
+
+// function reverse(str) {
+//   if (!str.length) return str;
+//   return reverse(str.slice(1)) + str.slice(0, 1);
+// }
+
+console.log(reverse('hello'));
 
 module.exports = reverse;
