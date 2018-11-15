0cd4adc69123051e2129a673152d540740421437
Gray Smith <gray.smith00@gmail.com>
2018-11-15 08:50:08 -0700

    Complete algocasts/fizzBuzz

--- algocasts/exercises/fizzbuzz/index.js
+++ algocasts/exercises/fizzbuzz/index.js
@@ -12,6 +12,37 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    switch (true) {
+      case i % 15 === 0:
+        console.log('fizzbuzz');
+        break;
+      case i % 3 === 0:
+        console.log('fizz');
+        break;
+      case i % 5 === 0:
+        console.log('buzz');
+        break;
+      default:
+        console.log(i);
+        break;
+    }
+  }
+}
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
 
 module.exports = fizzBuzz;
