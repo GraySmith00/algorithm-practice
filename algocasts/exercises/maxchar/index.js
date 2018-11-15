e36001931fe60848c256668ea541f3ed6152cdbd
Gray Smith <gray.smith00@gmail.com>
2018-11-14 20:07:57 -0700

    Complete algocasts/maxChar

--- algocasts/exercises/maxchar/index.js
+++ algocasts/exercises/maxchar/index.js
@@ -5,6 +5,36 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const counts = str.split('').reduce((counts, char) => {
+//     if (!counts[char]) counts[char] = 0;
+//     counts[char]++;
+//     return counts;
+//   }, {});
+
+//   return Object.keys(counts).reduce((maxChar, key, i) => {
+//     if (i === 0 || counts[key] > counts[maxChar]) maxChar = key;
+//     return maxChar;
+//   }, '');
+// }
+
+function maxChar(str) {
+  const counts = {};
+  let maxVal = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    counts[char] = counts[char] + 1 || 1;
+  }
+
+  for (let key in counts) {
+    if (counts[key] > maxVal) {
+      maxVal = counts[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
