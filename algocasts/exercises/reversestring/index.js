d43b2e3435c143c5f9b49009a45e7f6683f71c14
Gray Smith <gray.smith00@gmail.com>
2018-11-14 18:49:53 -0700

    Complete algocasts/reversestring

--- algocasts/exercises/reversestring/index.js
+++ algocasts/exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
 
 module.exports = reverse;
