dff380279034ee08a96238c1def4e33a5a246f70
Gray Smith <gray.smith00@gmail.com>
2018-11-15 22:08:37 -0700

    Complete algocasts/pyramid

--- algocasts/exercises/pyramid/index.js
+++ algocasts/exercises/pyramid/index.js
@@ -14,6 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// SOLUTION 1
+
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+// SOLUTION 2 (RECURSIVE)
+
+function pyramid(n, row = 0, level = '') {
+  if (row === n) return;
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    pyramid(n, row + 1);
+    return;
+  }
+
+  const mid = Math.floor((2 * n - 1) / 2);
+  if (mid - row <= level.length && mid + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  pyramid(n, row, level);
+}
+
+pyramid(3);
 
 module.exports = pyramid;
