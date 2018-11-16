a36313aef312910ac57a1e329833231322d2d108
Gray Smith <gray.smith00@gmail.com>
2018-11-16 12:35:04 -0700

    Complete algocasts/fib with memoization

--- algocasts/exercises/fib/index.js
+++ algocasts/exercises/fib/index.js
@@ -8,6 +8,54 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   const fibSeq = [0, 1];
+//   for (let i = 1; i < n; i++) {
+//     const num = fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length - 2];
+//     fibSeq.push(num);
+//   }
+//   return fibSeq[fibSeq.length - 1];
+// }
+
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
+
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+// function fibSeq(n) {
+//   if (n < 2) return [0, 1];
+//   const prev = fibSeq(n - 1);
+//   const num = prev[prev.length - 1] + prev[prev.length - 2];
+//   return [...prev, num];
+// }
+
+// console.log(fib(10));
+// console.log(fibSeq(10));
 
 module.exports = fib;
