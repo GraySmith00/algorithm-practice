181768d9cec84d713116b3428807f6347d5423ff
Gray Smith <gray.smith00@gmail.com>
2018-11-15 18:28:14 -0700

    Complete algocasts/capitalize

--- algocasts/exercises/capitalize/index.js
+++ algocasts/exercises/capitalize/index.js
@@ -7,6 +7,31 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+    .join(' ');
+}
+
+// function capitalize(str) {
+//   const words = [];
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ');
+// }
+
+// function capitalize(str) {
+//   let result = str.charAt(0).toUpperCase();
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
 
 module.exports = capitalize;
