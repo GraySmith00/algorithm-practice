87a5f4b988bc674bc21b4ad2e4a53d9c5d509a8d
Gray Smith <gray.smith00@gmail.com>
2018-11-15 22:27:47 -0700

    Complete algocasts/vowels

--- algocasts/exercises/vowels/index.js
+++ algocasts/exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   const v = 'aeiou'.split('');
+//   return str.split('').reduce((total, char) => {
+//     if (v.includes(char.toLowerCase())) total++;
+//     return total;
+//   }, 0);
+// }
+
+// function vowels(str) {
+//   let count = 0;
+//   for (let char of str.toLowerCase()) {
+//     if ('aeiou'.includes(char)) count++;
+//   }
+//   return count;
+// }
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
