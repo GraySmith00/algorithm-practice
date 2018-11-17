c3d3ea0fd6e228d01c17540e14b1b9ceaaa499ce
Gray Smith <gray.smith00@gmail.com>
2018-11-17 15:22:00 -0700

    Complete algocasts/qfroms

--- algocasts/exercises/qfroms/index.js
+++ algocasts/exercises/qfroms/index.js
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackA = new Stack();
+    this.stackB = new Stack();
+  }
+
+  add(record) {
+    this.stackA.push(record);
+  }
+
+  remove() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+
+    const record = this.stackB.pop();
+
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.stackA.peek()) {
+      this.stackB.push(this.stackA.pop());
+    }
+
+    const peek = this.stackB.peek();
+
+    while (this.stackB.peek()) {
+      this.stackA.push(this.stackB.pop());
+    }
+
+    return peek;
+  }
+}
 
 module.exports = Queue;
