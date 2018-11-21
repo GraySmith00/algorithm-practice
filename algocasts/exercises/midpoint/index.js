fa539a2cad125ce02ec691c00021c63e65139229
Gray Smith <gray.smith00@gmail.com>
2018-11-20 18:32:54 -0600

    Complete algocasts/midpoint

--- algocasts/exercises/midpoint/index.js
+++ algocasts/exercises/midpoint/index.js
@@ -12,6 +12,16 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
