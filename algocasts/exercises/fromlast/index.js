3986705d0cea9518b8d66d20f04cd38e0e23e9d4
Gray Smith <gray.smith00@gmail.com>
2018-11-22 11:31:03 -0500

    Complete algocasts/tree

--- algocasts/exercises/fromlast/index.js
+++ algocasts/exercises/fromlast/index.js
@@ -11,6 +11,20 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.head;
+  let fast = list.head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
--- practice/genPracticeOne.js
+++ practice/genPracticeOne.js
@@ -1,4 +1,6 @@
+// ================================================================
 // find out whether a number is prime
+// ================================================================
 const isPrime = n => {
   if (n % 2 === 0 || n < 2) return false;
   for (let i = 2; i < Math.sqrt(n); i++) {
@@ -11,7 +13,9 @@ const isPrime = n => {
 
 console.log(isPrime(179430413));
 
+// ================================================================
 // find the second highest number in an array
+// ================================================================
 const nums = [2, 4, 24, 6, 8, 19, 22, 31, 4, 6, 12];
 
 // const secondHighest = arr => {
@@ -34,7 +38,9 @@ const secondHighest = arr => {
 
 console.log(secondHighest(nums));
 
+// ================================================================
 // find the most frequently occuring string in an array
+// ================================================================
 const strings = [
   'the',
   'puppy',
@@ -63,7 +69,9 @@ const getMostFrequent = arr => {
 
 console.log(getMostFrequent(strings));
 
+// ================================================================
 // greatest common divisor of two nums
+// ================================================================
 const getCommonDivisor = (n1, n2) => {
   const highest = Math.max(n1, n2);
   let divisor = 1;
@@ -79,7 +87,9 @@ const getCommonDivisor = (n1, n2) => {
 
 console.log(getCommonDivisor(50, 65));
 
+// ================================================================
 // return fibonacci seq at nth num
+// ================================================================
 // const fib = n => {
 //   const seq = [0, 1];
 //   if (n < 2) return [0, 1];
@@ -111,7 +121,9 @@ const fib = memoize(slowFib);
 
 console.log(fib(50));
 
+// ================================================================
 // find n factorial
+// ================================================================
 // const slowFactorial = n => {
 //   if (n < 2) return 1;
 //   return n * factorial(n - 1);
@@ -131,7 +143,9 @@ console.log(fib(50));
 
 // console.log(factorial(4));
 
+// ================================================================
 // remove duplicates from an array
+// ================================================================
 const nums2 = [1, 4, 5, 3, 6, 3, 7, 4, 'three', 3, 5, 7, 8, 9];
 
 const removeDups = arr => {
@@ -145,7 +159,9 @@ const removeDups = arr => {
 
 console.log(removeDups(nums2));
 
+// ================================================================
 // reverse a string
+// ================================================================
 // const revStr = str => {
 //   let newStr = '';
 //   for (let i = str.length - 1; i >= 0; i--) {
@@ -159,3 +175,30 @@ const revStr = str => {
 };
 
 console.log(revStr('thanksgiving'));
+
+// ================================================================
+// Matrix
+// ================================================================
+const matrix = n => {
+  const result = [];
+  let counter = 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = counter;
+      counter++;
+    }
+  }
+
+  return result;
+};
+
+console.log(matrix(3));
