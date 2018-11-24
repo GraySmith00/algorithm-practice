28519b6786084c30246e88f2cd844812384870b2
Gray Smith <gray.smith00@gmail.com>
2018-11-24 11:16:40 -0500

    Complete algocasts/levelWidth

--- algocasts/exercises/levelwidth/index.js
+++ algocasts/exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let counters = [0];
+  let nodesArr = [root, 's'];
+
+  while (nodesArr.length > 1) {
+    const node = nodesArr.shift();
+    if (node === 's') {
+      counters.push(0);
+      nodesArr.push(node);
+    } else {
+      nodesArr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
