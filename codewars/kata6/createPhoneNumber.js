53e66af133d12230935ae5e33576f0036fdaca1d
Gray Smith <gray.smith00@gmail.com>
2018-11-25 21:40:26 -0700

    Complete codewars/kata6/createPhoneNumber

--- codewars/kata6/createPhoneNumber.js
+++ codewars/kata6/createPhoneNumber.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/create-phone-number/train/javascript
+
 // function createPhoneNumber(numbers){
 //   const str = numbers.join('');
 //   return `(${str.slice(0, 3)}) ${str.slice(3, 6)} - ${str.slice(6)}`
