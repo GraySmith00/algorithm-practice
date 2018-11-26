2597d026581672d8c078fb0b786963624c7f54d2
Gray Smith <gray.smith00@gmail.com>
2018-11-25 21:11:32 -0700

    Complete algocasts/mergeSort

--- algocasts/exercises/sorting/index.js
+++ algocasts/exercises/sorting/index.js
@@ -32,8 +32,26 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const midIndex = Math.floor(arr.length / 2);
+  const left = arr.slice(0, midIndex);
+  const right = arr.slice(midIndex);
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  const results = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];
+}
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+mergeSort([5, 2]);
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
