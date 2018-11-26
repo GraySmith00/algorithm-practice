377c335bc481f9b831b1d96984f6215d12bf6a13
Gray Smith <gray.smith00@gmail.com>
2018-11-25 21:11:51 -0700

    Complete codewars/kata6/createPhoneNumber

---
+++ codewars/kata6/createPhoneNumber.js
@@ -0,0 +1,10 @@
+// function createPhoneNumber(numbers){
+//   const str = numbers.join('');
+//   return `(${str.slice(0, 3)}) ${str.slice(3, 6)} - ${str.slice(6)}`
+// }
+
+function createPhoneNumber(numbers) {
+  return numbers.reduce((p, c) => p.replace('x', c), '(xxx) xxx-xxxx');
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // "(123) 456-7890";
