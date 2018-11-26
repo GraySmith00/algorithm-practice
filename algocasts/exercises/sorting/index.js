4cd1dc3ce67a599e852f8d4d74d69c7d8e5e5b38
Gray Smith <gray.smith00@gmail.com>
2018-11-25 17:37:12 -0700

    Complete algocasts/bubblesort

--- algocasts/exercises/sorting/index.js
+++ algocasts/exercises/sorting/index.js
@@ -2,19 +2,25 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  let swapped;
+  for (let i = 0; i < arr.length; i++) {
+    if (swapped === false) return arr;
+    swapped = false;
 
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
